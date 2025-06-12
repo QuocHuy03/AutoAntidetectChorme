72e347f62f9d6cfe47baa05b9a61b9da58f482f5
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2025-06-12 10:15:46 +0700

    fx

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-proxies = {
-    "http": "http://sundayokwu12345:c2Vw96fzFP@216.180.253.133:50101",
-    "https": "http://sundayokwu12345:c2Vw96fzFP@216.180.253.133:50101"
-}
-
-r = requests.get("https://ipinfo.io/ip", proxies=proxies, timeout=5)
-print(r.text)
